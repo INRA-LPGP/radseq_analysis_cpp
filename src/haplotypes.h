76f7b774d2362e0411526e0c8d99b5506d4d8001
Romain Feron <romain.feron@evobio.eu>
2017-05-30 14:21:34 +0200

    Lower threshold to retain loci

--- src/haplotypes.h
+++ src/haplotypes.h
@@ -4,7 +4,7 @@
 #include <bitset>
 
 #define BIT_SIZE 64 // Length of bitset is multiple of 32. Assumption: number of males < 64. If number of males is higher, can be increased to 96 or 128.
-#define MALE_MAX (BIT_SIZE - 10) // Threshold to filter haplotypes: if less than 3 original males diverge from females, haplotype is discarded.
+#define MALE_MAX (BIT_SIZE - 5) // Threshold to filter haplotypes: if less than 10 original males diverge from females, haplotype is discarded.
 
 struct Infos {
     std::vector<bool> columns;
